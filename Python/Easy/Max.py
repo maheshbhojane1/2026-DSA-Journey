83ea720cb24ac1263214ae53b3d213b49c0ebb43
Mahesh Bhojane <maheshbhojane200@gmail.com>
2026-01-10 18:51:46 +0530

    python

---
+++ Python/Easy/Max.py
@@ -0,0 +1,12 @@
+# def max(list):
+#     if not list:
+#         return None  
+#     maximum = list[0]
+#     for num in list:
+#         if num > maximum:
+#             maximum = num
+#     return maximum
+
+# numbers = [3, 1, 4, 1, 5, 9]
+# print("The maximum value is:", max(numbers)) 
+
---
+++ Python/Easy/Reversed.py
@@ -0,0 +1,7 @@
+
+
+def reverse(string: str):
+    return string[::-1] 
+text = "Hello, World!"
+print("The reversed string is:", reverse(text))
+
