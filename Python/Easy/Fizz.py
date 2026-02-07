7a064d2f703c83bf47e638e6e5a0c42086ef0df9
Mahesh Bhojane <maheshbhojane200@gmail.com>
2026-02-07 20:28:08 +0530

    update python

--- Python/Easy/Fizz.py
+++ Python/Easy/Fizz.py
@@ -1,9 +1,21 @@
-for i in range(1, 20):
-    result = ""
+# for i in range(1, 20):
+#     result = ""
     
-    if i % 3 == 0:
-        result += "Fizz"
-    if i % 5 == 0:
-        result += "Buzz"
+#     if i % 3 == 0:
+#         result += "Fizz"
+#     if i % 5 == 0:
+#         result += "Buzz"
         
-    print(result or i)
+#     print(result or i)
+
+def oddEven(n):
+    if n % 2 ==0:
+        print("Not Wired")
+    else:
+        print("Wired")
+
+
+
+
+oddEven(5)
+        
---
+++ Python/Easy/Number.py
@@ -0,0 +1,5 @@
+
+n = int(input("Enter a number: ").strip())
+
+for i in range(n):
+    print(i * i)
