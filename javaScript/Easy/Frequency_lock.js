9552993f1f46c09b79316e3573f416bbaf3c1dbc
Mahesh Bhojane <maheshbhojane200@gmail.com>
2026-02-09 21:51:23 +0530

    09-02-2026

---
+++ javaScript/Easy/Frequency_lock.js
@@ -0,0 +1,23 @@
+function frequencyLock(arr1, arr2, arr3) {
+    let set1 = new Set(arr1);
+    let set2 = new Set(arr2);
+    let result = [];
+
+    for (let num of arr3) {
+        if (set1.has(num) && set2.has(num)) {
+            result.push(num);
+        }
+    }
+
+    return [...new Set(result)]; // remove duplicates
+}
+
+// Example
+console.log(frequencyLock(
+    [1, 2, 3, 4],
+    [2, 3, 5],
+    [2, 3, 6]
+)); 
+// Output: [2, 3]
+
+
